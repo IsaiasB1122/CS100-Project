d8b4f74da04f0abfb0f011f0ee85bc16e953616f
rselfucr <rself001@ucr.edu>
2024-11-13 12:31:56 -0800

    Remove unused path members of Directory
    > Still remaining to be refactored: File reading exceptions

--- include/Directory.hpp
+++ include/Directory.hpp
@@ -34,13 +34,6 @@ public:
     class invalid_path : std::runtime_error {using std::runtime_error::runtime_error;};
     class unsupported_version : std::runtime_error {using std::runtime_error::runtime_error;};
     CategoryList default_categories;
-    // fspath get_metadata_path();
-    // fspath get_boards_path();
-    // fspath get_board_path(TaskBoard* board);
-    static bool exists_at_path(std::string path);
-    static Directory load_from_path(std::string path);
-    static Directory new_at_path(std::string path, std::string name);
-    std::string get_path();
     std::string get_name();
     TaskBoard* add_board(std::string name);
     void remove_board(TaskBoard* board);
