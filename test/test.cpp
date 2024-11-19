5b4dc365dce84fc0f6755fa385c357c3e0c66a2f
rselfucr <rself001@ucr.edu>
2024-11-18 16:18:43 -0800

    Set up unit tests building

---
+++ test/test.cpp
@@ -0,0 +1,5 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include <lib/file_io.hpp>
+
+#include <test_fileio.cpp>
---
+++ test/test_fileio.cpp
@@ -0,0 +1,4 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include <lib/file_io.hpp>
+
