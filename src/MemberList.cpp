b674b6b134c5094b38dcb2e97ecf74323435fd8d
crisapi <apicristian@icloud.com>
2024-11-20 09:28:58 -0800

    "Added get members function and add members function"

--- src/MemberList.cpp
+++ src/MemberList.cpp
@@ -10,3 +10,41 @@ MemberList::~MemberList() {
         delete c;
     }
 }
+
+const std::vector<Member*> MemberList::get_members() {
+    std::vector<Member*> members;
+    for (auto m : this->members) members.push_back(m);
+    return members;
+}
+
+const Member& MemberList::get_member(uint32_t id) {
+    for (auto m = this->members.begin(); m < members.end(); m += 1) { 
+        if ((m*)->id == id) return *m;
+    }
+    return nullptr;
+}
+
+const Member& MemberList::get_member(std::string name) {
+    for (auto m = this->members.begin(); m < members.end(); m += 1) { 
+        if ((m*)->name == name) return *m;
+    }
+    return nullptr;
+}
+
+const Member& MembersList::add_member(Member category) {
+    Members* member = new Member(this);
+    member->name = name;
+    member->id = next_members_id;
+    this->next_members_id += 1;
+
+    return member;
+}
+
+void remove_member(uint32_t id) {
+    for (auto m = this->members.begin(); m < this->members.end(); m += 1) {
+        if (m*->id == id) {
+            this->members.erase(m);
+            delete member;
+        }
+    }
+}
