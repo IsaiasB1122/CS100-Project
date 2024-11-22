64cbb179a182d9a08e9ff9682b36da31fab6f680
IsaiasB1122 <isaiasbernal2211@gmail.com>
2024-11-21 16:15:22 -0800

    using filter_data now

--- src/commands/List-Boards.cpp
+++ src/commands/List-Boards.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <Commands.hpp>
 #include <command_help.hpp>
 #include <Directory.hpp>
+#include <TaskBoard.hpp>
 #include <lib/dir_helpers.hpp>
 using namespace std;
 
@@ -17,18 +19,31 @@ class CommandListBoards : public Command {
         vector<string> get_required_parameters() {return {};};
     	vector<string> get_optional_parameters() {return {"filter"};};
 
-        CommandManager::COMMAND_RUN_RESULT run(CommandParametersData parameters, std::ostream& out){		
-            vector<TaskBoard*>  boards = this->parent->dir->get_boards();
-            string filter;
-            if (parameters.has_parameter("filter")) {
-                filter = parameters.get_parameter("filter");
+        CommandManager::COMMAND_RUN_RESULT run(CommandParametersData parameters, ostream& out){		
+            string filter = parameters.get_parameter("filter");
+            vector <TaskBoard*> boards = this->parent->dir->get_boards();
+            vector<DataEntry*> boards_as_data_entry;
+
+            for (auto board : boards) {
+                boards_as_data_entry.push_back(static_cast<DataEntry*>(board));
             }
-            for (TaskBoard* board : boards){
-                if (board != nullptr && (filter.empty() || board->to_string().find(filter) != string::npos)) {
-                    out << board->to_string() << endl;
+             
+            vector<DataEntry*> filteredBoards = DataEntry::filter_data(boards_as_data_entry, filter, DataEntry::SORT_TYPE::NONE);
+            if (filteredBoards.empty()) {
+                out << "No boards match the filter provided." << endl;
+            }
+            else {
+                for (auto board : filteredBoards) {
+                    //TaskBoard* itr = dynamic_cast<TaskBoard*>(board);
+                    if(board) {
+                        out << board->to_string() << endl;
+                    }
+                    else {
+                        out << "Failed to cast DataEntry to TaskBoard" << endl;
+                    }
                 }
             }
-            
-            return CommandManager::COMMAND_RUN_RESULT::GOOD;	    		
-        } 
+            return CommandManager::COMMAND_RUN_RESULT::GOOD;
+    }
+    
 };
