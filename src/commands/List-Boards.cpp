1e4ea50cbcf8cbdff7f26589b37cfc8130865152
crisapi <apicristian@icloud.com>
2024-11-28 12:05:58 -0800

    Attempt to fix merge conflict

--- src/commands/List-Boards.cpp
+++ src/commands/List-Boards.cpp
@@ -21,5 +21,34 @@ class CommandListBoards : public Command {
 
         CommandManager::COMMAND_RUN_RESULT run(CommandParametersData parameters, ostream& out) {		
             string filter = parameters.get_parameter("filter");
+            vector<TaskBoard*> boards = this->parent->dir->get_boards();
+
+            if (boards.empty()) {
+                out << "There are no boards available for this directory" << endl;
+                return CommandManager::COMMAND_RUN_RESULT::ERROR;
+            }
+
+
+            vector<DataEntry*> boards_as_data_entry;
+            for (auto board : boards) {
+                boards_as_data_entry.push_back(static_cast<DataEntry*>(board));
+            }
+
+            vector<DataEntry*> filteredBoards = DataEntry::filter_data(boards_as_data_entry, filter, DataEntry::SORT_TYPE::NONE);
+            if (filteredBoards.empty()) {
+                out << "No boards match the filter: " << filter << endl;
+                return CommandManager::COMMAND_RUN_RESULT::ERROR;
+            }
+
+            for (auto board : filteredBoards) {
+                if (board != nullptr) {
+                    out << board->to_string() << endl;
+                } else {
+                    out << "Failed to get boards" << endl;
+                    return CommandManager::COMMAND_RUN_RESULT::ERROR;
+                }
+            }
+
+            return CommandManager::COMMAND_RUN_RESULT::GOOD;
         }
 };
