eff5b67202005a17a2c36c872a0b66cf5dea31e7
crisapi <apicristian@icloud.com>
2024-11-28 12:15:22 -0800

    Reverted

--- src/commands/List-Boards.cpp
+++ src/commands/List-Boards.cpp
@@ -19,36 +19,31 @@ class CommandListBoards : public Command {
         vector<string> get_required_parameters() {return {};};
     	vector<string> get_optional_parameters() {return {"filter"};};
 
-        CommandManager::COMMAND_RUN_RESULT run(CommandParametersData parameters, ostream& out) {		
+        CommandManager::COMMAND_RUN_RESULT run(CommandParametersData parameters, ostream& out){		
             string filter = parameters.get_parameter("filter");
-            vector<TaskBoard*> boards = this->parent->dir->get_boards();
+            vector <TaskBoard*> boards = this->parent->dir->get_boards();
+            vector <DataEntry*> boards_as_data_entry;
 
-            if (boards.empty()) {
-                out << "There are no boards available for this directory" << endl;
-                return CommandManager::COMMAND_RUN_RESULT::ERROR;
-            }
-
-
-            vector<DataEntry*> boards_as_data_entry;
             for (auto board : boards) {
                 boards_as_data_entry.push_back(static_cast<DataEntry*>(board));
             }
-
+             
             vector<DataEntry*> filteredBoards = DataEntry::filter_data(boards_as_data_entry, filter, DataEntry::SORT_TYPE::NONE);
             if (filteredBoards.empty()) {
-                out << "No boards match the filter: " << filter << endl;
+                out << "No boards have: " << filter <<  endl;
                 return CommandManager::COMMAND_RUN_RESULT::ERROR;
             }
-
-            for (auto board : filteredBoards) {
-                if (board != nullptr) {
-                    out << board->to_string() << endl;
-                } else {
-                    out << "Failed to get boards" << endl;
-                    return CommandManager::COMMAND_RUN_RESULT::ERROR;
+            else {
+                for (auto board : filteredBoards) {
+                    if(board != nullptr) {
+                        out << board->to_string() << endl;
+                    }
+                    else {
+                        out << "Failed to get boards" << endl;
+                        return CommandManager::COMMAND_RUN_RESULT::ERROR;
+                    }
                 }
             }
-
             return CommandManager::COMMAND_RUN_RESULT::GOOD;
-        }
+    }
 };
