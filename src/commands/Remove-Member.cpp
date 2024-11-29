6c372eb5a8384d268b2697df75940bacd6bb0088
crisapi <apicristian@icloud.com>
2024-11-28 21:36:14 -0800

    Remove member implementation

--- src/commands/Remove-Member.cpp
+++ src/commands/Remove-Member.cpp
@@ -14,7 +14,35 @@ public:
     std::vector<std::string> get_optional_parameters() {return {};};
 
     CommandManager::COMMAND_RUN_RESULT run(CommandParametersData parameters, std::ostream& out) {
-        out << "remove member" << std::endl;
+        TaskBoard* board = get_board(*this->parent->dir, parameters.get_parameter("board"));
+        if (board == nullptr) {
+            out << "ERROR: Board [" << parameters.get_parameter("board") << "] is not found." << std::endl;
+            return CommandManager::COMMAND_RUN_RESULT::ERROR;
+        }
+
+        std::string member_name = parameters.get_parameter("member");
+        Member* member = nullptr;
+
+        try {
+            member = &board->members.get_member(member_name);
+        } catch (const std::runtime_error& e) {
+            out << "ERROR: Member [" << member_name << "] not found on board [" << parameters.get_parameter("board") << "]." << std::endl;
+            return CommandManager::COMMAND_RUN_RESULT::ERROR;
+        }
+
+
+        uint32_t member_id = member->id;
+        std::string member_string = member->to_string();
+
+
+        board->members.remove_member(member_id);
+        board->members_changed = true;
+        FileIOManager::taskboard_write(*board);
+        FileIOManager::directory_write_metadata(*this->parent->dir);
+
+
+        out << "REMOVE MEMBER " << member_string << std::endl;
+
         return CommandManager::COMMAND_RUN_RESULT::GOOD;
     }
 };
