9c743125058f8ed5da8a3015dab8b5feb7753d6d
crisapi <apicristian@icloud.com>
2024-11-30 19:48:28 -0800

    Added taskboard_write and added better exceptions.

--- src/commands/Assign-Member.cpp
+++ src/commands/Assign-Member.cpp
@@ -16,25 +16,35 @@ public:
     CommandManager::COMMAND_RUN_RESULT run(CommandParametersData parameters, std::ostream& out) {
         TaskBoard* board = get_board(*this->parent->dir, parameters.get_parameter("board"));
         if (board == nullptr) {
-            throw std::runtime_error("ERROR: Board [" + parameters.get_parameter("board") + "] is not found.");
+            throw std::runtime_error("Board [" + parameters.get_parameter("board") + "] not found.");
         }
 
-        try {
         std::string task_name = parameters.get_parameter("task");
-        Task task = get_task(*board, task_name); 
+        Task task;
+        try {
+            task = get_task(*board, task_name); 
+        } catch (const std::exception& e) {
+            out << "Error: Task [" << task_name << "] not found." << std::endl;
+            return CommandManager::COMMAND_RUN_RESULT::ERROR;
+        }
 
         std::string member_name = parameters.get_parameter("member");
-        Member member_to_assign = get_member(*board, member_name); 
+        Member member_to_assign;
+
+        try {
+            member_to_assign = get_member(*board, member_name);
+        } catch (const std::exception& e) {
+            out << "Error: Member [" << member_name << "] not found." << std::endl;
+            return CommandManager::COMMAND_RUN_RESULT::ERROR;
+        }
 
         uint32_t id = member_to_assign.id;
         task.assigned_members.push_back(id);
+        task.changed = true;
 
+        FileIOManager::taskboard_write(*board);
         out << "ASSIGN " << member_to_assign.to_string() << " to " << task.to_string(*board) << std::endl;
 
         return CommandManager::COMMAND_RUN_RESULT::GOOD;
-    } catch (const std::exception& e) {
-        out << "ERROR: " << e.what() << std::endl;
-        return CommandManager::COMMAND_RUN_RESULT::ERROR;
-        }
     }
 };
