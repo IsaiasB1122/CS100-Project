3c7be5011f3e3c2196d3e9359e2a562f15f91957
Nhin0928 <dnguy597@ucr.edu>
2024-11-30 23:34:31 -0800

    Fix list-categories by removing filter and using to_string

--- src/commands/List-Categories.cpp
+++ src/commands/List-Categories.cpp
@@ -13,7 +13,7 @@ public:
         return COMMAND_HELP_LIST_CATEGORIES;
     }
     std::vector<std::string> get_required_parameters() {return {"board"};};
-    std::vector<std::string> get_optional_parameters() {return {"filter"};};
+    std::vector<std::string> get_optional_parameters() {return {};};
 
     CommandManager::COMMAND_RUN_RESULT run(CommandParametersData parameters, std::ostream& out) {
         // Work
@@ -25,7 +25,7 @@ public:
         const std::vector<CategoryInfo*> categories = board->get_categories();
     
         for (const auto& category : categories) {
-            out << "[" << category->id << "\t" << category->name << "]" << std::endl;
+            out << category->to_string() << std::endl;
         }        
         return CommandManager::COMMAND_RUN_RESULT::GOOD;
     }
