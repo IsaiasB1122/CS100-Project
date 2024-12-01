33c00cba77c5dab2ddffa4588c3710bc2d1265ed
Nhin0928 <dnguy597@ucr.edu>
2024-11-30 22:37:52 -0800

    Implement list categories

--- src/commands/List-Categories.cpp
+++ src/commands/List-Categories.cpp
@@ -1,5 +1,7 @@
 #include <Commands.hpp>
-
+#include <TaskBoard.hpp>
+#include <lib/file_io.hpp>
+#include <lib/dir_helpers.hpp>
 #include <iostream>
 
 class CommandListCategories : public Command {
@@ -14,7 +16,18 @@ public:
     std::vector<std::string> get_optional_parameters() {return {"filter"};};
 
     CommandManager::COMMAND_RUN_RESULT run(CommandParametersData parameters, std::ostream& out) {
-        out << "list category" << std::endl;
+        // Work
+        TaskBoard* board = get_board(*this->parent->dir, parameters.get_parameter("board"));
+        if (board == nullptr) {
+            out << "ERROR: Board [" << parameters.get_parameter("board") << "] not found." << std::endl;
+            return CommandManager::COMMAND_RUN_RESULT::ERROR;
+        }
+        const std::vector<CategoryInfo*> categories = board->get_categories();
+    
+        for (const auto& category : categories) {
+            out << "[" << category->id << "\t" << category->name << "]" << std::endl;
+        }        
         return CommandManager::COMMAND_RUN_RESULT::GOOD;
     }
+
 };
