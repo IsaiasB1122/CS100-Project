c28f19e152fa46276d20c07159f64f93be5f8b2c
Nhin0928 <dnguy597@ucr.edu>
2024-12-01 23:50:18 -0800

    Implement Categorize tasks

--- src/commands/Categorize-Task.cpp
+++ src/commands/Categorize-Task.cpp
@@ -1,5 +1,7 @@
 #include <Commands.hpp>
-
+#include <TaskBoard.hpp>
+#include <lib/file_io.hpp>
+#include <lib/dir_helpers.hpp>
 #include <iostream>
 
 class CommandCategorizeTask : public Command {
@@ -14,7 +16,53 @@ public:
     std::vector<std::string> get_optional_parameters() {return {};};
 
     CommandManager::COMMAND_RUN_RESULT run(CommandParametersData parameters, std::ostream& out) {
-        out << "categorize task" << std::endl;
+        TaskBoard* board = get_board(*this->parent->dir, parameters.get_parameter("board"));
+    if (board == nullptr) {
+        out << "ERROR: Board [" << parameters.get_parameter("board") << "] is not found." << std::endl;
+        return CommandManager::COMMAND_RUN_RESULT::ERROR; 
+    }
+    const Task* task = nullptr;
+    
+    try {
+        if (parameters.has_parameter("task")) {
+            uint32_t task_id = std::stoi(parameters.get_parameter("task"));
+            task = &board->get_task(task_id);
+        } else if (parameters.has_parameter("name")) {
+            task = &board->get_task(parameters.get_parameter("name"));
+        } else {
+            throw std::invalid_argument("Task name or ID is required.");
+        }
+    } catch (const std::invalid_argument&) {
+        out << "ERROR: Task \"" << parameters.get_parameter("name") << "\" not found on board." << std::endl;
+        return CommandManager::COMMAND_RUN_RESULT::ERROR;
+    }
+
+    // Get the category
+    CategoryInfo* category = nullptr;
+    const std::vector<CategoryInfo*> categories = board->get_categories();
+    for (auto& cat : categories) {
+        if (cat->name == parameters.get_parameter("category") || 
+            std::to_string(cat->id) == parameters.get_parameter("category")) {
+            category = cat;
+            break;
+        }
+    }
+
+    if (category == nullptr) {
+        out << "ERROR: Category \"" << parameters.get_parameter("category") 
+            << "\" does not match any Category name or ID of board [" 
+            << board->get_categories().size() << "\t" << parameters.get_parameter("board") << "]." << std::endl;
+        return CommandManager::COMMAND_RUN_RESULT::ERROR;
+    }
+    
+    std::string old_category_name = board->categories.get_category(task->category_id).name;
+
+    // Update the task's category
+    uint32_t old_category_id = task->category_id;
+    board->move_task(task->id, category->id);
+
+    // Generate output
+    out << "CATEGORIZE TASK" << task->to_string(*board) << " " << old_category_name << "-->" << category->name << std::endl;
         return CommandManager::COMMAND_RUN_RESULT::GOOD;
     }
 };
