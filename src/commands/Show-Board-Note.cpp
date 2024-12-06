e866f4beeece45f48092d5a0d8a10c65bfc13cf2
rselfucr <rself001@ucr.edu>
2024-12-05 19:54:53 -0800

    Implement show-board-note

--- src/commands/Show-Board-Note.cpp
+++ src/commands/Show-Board-Note.cpp
@@ -1,5 +1,7 @@
 #include <Commands.hpp>
 
+#include <lib/dir_helpers.hpp>
+#include <lib/file_io.hpp>
 #include <iostream>
 
 class CommandShowBoardNote : public Command {
@@ -14,7 +16,25 @@ public:
     std::vector<std::string> get_optional_parameters() {return {};};
 
     CommandManager::COMMAND_RUN_RESULT run(CommandParametersData parameters, std::ostream& out) {
-        out << "show board note" << std::endl;
+        // Resolve board
+        TaskBoard* board = get_board(*this->parent->dir, parameters.get_parameter("board"));
+        if (board == nullptr) {
+            out << "ERROR: Board [" << parameters.get_parameter("board") << "] is not found." << std::endl;
+            return CommandManager::COMMAND_RUN_RESULT::ERROR; 
+        }
+        // Resolve note
+        const Note* note = 0;
+        try {
+            note = &get_board_note(*board,parameters.get_parameter("note"));
+        }
+        catch(const std::invalid_argument e)
+        {
+            out << "ERROR: Note [" << parameters.get_parameter("note") << "] does not exist." << std::endl;
+            return CommandManager::COMMAND_RUN_RESULT::ERROR;
+        }
+
+        std::cout << note->to_string_full(*board) << std::endl;
+
         return CommandManager::COMMAND_RUN_RESULT::GOOD;
     }
 };
